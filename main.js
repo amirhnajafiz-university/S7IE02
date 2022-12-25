3e7f49fd1ab9e70d0b57a9ec7027b28267fbc424
amirhnajafiz <najafizadeh21@gmail.com>
2022-12-25 17:50:34 +0330

    fix: bugs

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 // errors
-const errNoInternetConnection = "No Internet Connection!";
+const errNoInternetConnection = "No internet connection!";
+const errUserNotFound = "User not found!"
 
 // messages
-const msgConnectToInternet = "Internet Connected."
+const msgConnectToInternet = "Internet connected.";
+const msgReadFromLocal = "Read from local storage."
 
 // elements
 let responseElement = document.getElementById("response");
+let userInputElement = document.getElementById("username");
 
 
 // add event listeners for checking internet connection.
@@ -65,116 +68,63 @@ function read_from_local(key){
     return localStorage.getItem(key);
 }
 
-        //================================================================================================================================
-        //=======================function to call for the user from either local_storage/cookies or github api============================
-        //================================================================================================================================
-        async function getUser(){
-            // function to call for the user from either local_storage/cookies or github api.
-            let username = document.getElementById("username").value
+// function to call for the user from either local storage/cookies or github api.
+async function getUser(){
+    // get username
+    let username = userInputElement.value
 
+    // check local storage
+    if (read_from_local(username) == null) {
+        // need to send http request.
+        let response = await fetch(`https://api.github.com/users/${username}`);
 
-            //================================================================================================================================
-            //================================================================================================================================
-            // Comment out the code block below if you don't want to use LOCAL_STORAGE
-            //--------------------------------------------------------------------------------------------------------------------------------
-            if (localStorage.getItem(username) == null) {
-                console.log("Saving to Local Storage");
-                let response = await fetch(`https://api.github.com/users/${username}`);
+        // send http request to github api
+        let data = await response.json();
+        data = JSON.stringify(data);
 
-                var data = await response.json();
+        // save it into local storage
+        save_to_local(username, data);
+        document.getElementById("message").innerHTML = "";
+    } else {
+        // no need for api request
+        var data = JSON.parse(read_from_local(username));
+        document.getElementById("message").innerHTML = msgReadFromLocal;
+    }
+    
+    // get data from cookie
+    if (getCookie(username) == "") {
+        let response = await fetch(`https://api.github.com/users/${username}`);
 
-                let s_data = JSON.stringify(data);
-                save_to_local(username, s_data);
-                document.getElementById("message").innerHTML = "";
-            } else {
-                console.log("Reading from Local Storage");
-                let s_data = read_from_local(username);
-                var data = JSON.parse(s_data);
-                document.getElementById("message").innerHTML = "Data Loaded from Local Storage";
-            }
-            //================================================================================================================================
-            //================================================================================================================================
-            //--------------------------------------------------------------------------------------------------------------------------------
-            //--------------------------------------------------------------------------------------------------------------------------------
-            //--------------------------------------------------------------------------------------------------------------------------------
-            //================================================================================================================================
-            //================================================================================================================================
-            // Comment out the code block below if you don't want to use COOKIES
-            //================================================================================================================================
-            //================================================================================================================================
-            if (getCookie(username) == "") {
-                console.log("Setting to Cookies");
-                console.log(getCookie(username) == "");
-                let response = await fetch(`https://api.github.com/users/${username}`);
+        var data = await response.json();
+        data = JSON.stringify(data);
 
-                var data = await response.json();
+        setCookie(username, data, 1);
+        document.getElementById("message").innerHTML = "";
+    } else {
+        var data = JSON.parse(getCookie(username));
+        document.getElementById("message").innerHTML = msgReadFromLocal;
+    }
 
-                let s_data = JSON.stringify(data);
-
-                setCookie(username, s_data, 1);
-                document.getElementById("message").innerHTML = "";
-            } else {
-                console.log("Getting from Cookies");
-                let s_data = getCookie(username);
-                var data = JSON.parse(s_data);
-                document.getElementById("message").innerHTML = "Data Loaded from Cookies";
-            }
-            //================================================================================================================================
-            //================================================================================================================================
-            //================================================================================================================================
-            //==============================Requesting Data from Github Apis, since Nothing Found in Locally==================================
-            //======================================and Setting Each Data into its Appropriate Place==========================================
-            //================================================================================================================================
-            if (data.message) {
-                document.getElementById("info-box").style.opacity = 0.35;
-                document.getElementById("message").innerHTML = "User Not Found | Try Again";
-            } else {
-                if (document.getElementById("message").innerHTML == "User Not Found | Try Again"){
-                    document.getElementById("message").innerHTML = "";
-                }
-                document.getElementById("info-box").style.opacity = 1;
-                if (data.avatar_url) {document.getElementById("avatar-img").src = data.avatar_url ;}
-                if (data.name) {document.getElementById("account-name").innerHTML = data.name} else {document.getElementById("account-name").innerHTML = "unknown"}
-                if (data.bio) {document.getElementById("bio").innerHTML = data.bio;} else {document.getElementById("bio").innerHTML = "No Bio"}
-                if (data.blog) {document.getElementById("blog").innerHTML = "blog: " + data.blog.replace('https://','www.');} else {document.getElementById("blog").innerHTML = "No Blog"}
-
-                if (data.location) {document.getElementById("loc").innerHTML = data.location} else {document.getElementById("loc").innerHTML = "location not specified"}
-                if (data.followers) {document.getElementById("followers").innerHTML = data.followers} else {document.getElementById("followers").innerHTML = "unknown"}
-                if (data.following) {document.getElementById("following").innerHTML = data.following} else {document.getElementById("following").innerHTML = "unknown"}
-            }
-            // ================================================================================================================================
-            // ================================================================================================================================
-            // ==================================Code Block Below Evaluate the Favourite Programming Language==================================
-            // ===================================================of the Searched User=========================================================
-            if (data.repos_url) {
-                let langs = [];
-                const response = fetch(data.repos_url);
-                response
-                .then((response) => response.json())
-                .then((repos) => {
-                    for (let index = 0;  index < repos.length; index++){
-                        let repo = repos[index];
-                        if (repo.language) {
-                            langs.push(repo.language);
-                        }
-                    }
-                    let count = {};
-                    langs.forEach(function(i) { count[i] = (count[i]||0) + 1;});
-                    let max = 0;
-                    let fav_lang = null;
-                    for (lang in count) {
-                        if (count[lang] > max) {
-                            max = count[lang];
-                            fav_lang = lang;
-                        }
-                    }
-                    if (fav_lang) {
-                        document.getElementById("fav_lang").innerHTML = fav_lang;
-                    } else {
-                        document.getElementById("fav_lang").innerHTML = 'Not Specified';
-                    }
-                });
-            }
-            // ================================================================================================================================
-            // ================================================================================================================================
+    if (data.message) {
+        document.getElementById("info-box").style.opacity = 0.35;
+        document.getElementById("message").innerHTML = errUserNotFound;
+    } else {
+        if (document.getElementById("message").innerHTML == errUserNotFound){
+            document.getElementById("message").innerHTML = "";
         }
+
+        document.getElementById("info-box").style.opacity = 1;
+
+        if (data.avatar_url) {document.getElementById("avatar-img").src = data.avatar_url ;}
+        if (data.name) {document.getElementById("account-name").innerHTML = data.name} else {document.getElementById("account-name").innerHTML = "unknown"}
+        if (data.bio) {document.getElementById("bio").innerHTML = data.bio;} else {document.getElementById("bio").innerHTML = "No Bio"}
+        if (data.blog) {document.getElementById("blog").innerHTML = "blog: " + data.blog.replace('https://','www.');} else {document.getElementById("blog").innerHTML = "No Blog"}
+
+        if (data.location) {document.getElementById("loc").innerHTML = data.location} else {document.getElementById("loc").innerHTML = "location not specified"}
+        if (data.followers) {document.getElementById("followers").innerHTML = data.followers} else {document.getElementById("followers").innerHTML = "unknown"}
+        if (data.following) {document.getElementById("following").innerHTML = data.following} else {document.getElementById("following").innerHTML = "unknown"}
+        if (data.company) {
+            document.getElementById("company").innerHTML = data.company
+        }
+    }
+}
